e6ca89299b51cb7257406c254459e03d8767c41a
Kermalis <29823718+Kermalis@users.noreply.github.com>
2023-04-22 19:08:15 -0400

    Add pitch helper methods

--- KMIDI/Events/PitchBendMessage.cs
+++ KMIDI/Events/PitchBendMessage.cs
@@ -5,6 +5,8 @@ namespace Kermalis.MIDI;
 
 public sealed class PitchBendMessage : MIDIMessage, IMIDIChannelMessage
 {
+	private const int CENTER = 0x2000;
+
 	public byte Channel { get; }
 
 	public byte LSB { get; }
@@ -42,6 +44,49 @@ public sealed class PitchBendMessage : MIDIMessage, IMIDIChannelMessage
 		Channel = channel;
 		LSB = lsb;
 		MSB = msb;
+	}
+	public PitchBendMessage(byte channel, int pitch)
+	{
+		Utils.ValidateMIDIChannel(channel);
+		GetLSBAndMSBFromPitchInt(pitch, out byte lsb, out byte msb);
+
+		Channel = channel;
+		LSB = lsb;
+		MSB = msb;
+	}
+
+	/// <summary>Returns a value in the range [-8_192, 8_191]</summary>
+	public int GetPitchAsInt()
+	{
+		uint uPitch = ((uint)MSB << 7) | LSB;
+		return (int)uPitch - CENTER;
+	}
+	/// <summary><inheritdoc cref="GetPitchAsInt()"/></summary>
+	public static int GetPitchAsInt(byte lsb, byte msb)
+	{
+		if (lsb > 127)
+		{
+			throw new ArgumentOutOfRangeException(nameof(lsb), lsb, null);
+		}
+		if (msb > 127)
+		{
+			throw new ArgumentOutOfRangeException(nameof(msb), msb, null);
+		}
+
+		uint uPitch = ((uint)msb << 7) | lsb;
+		return (int)uPitch - CENTER;
+	}
+	public static void GetLSBAndMSBFromPitchInt(int pitch, out byte lsb, out byte msb)
+	{
+		if (pitch is > (0x3FFF - CENTER) or < (0 - CENTER))
+		{
+			throw new ArgumentOutOfRangeException(nameof(pitch), pitch, "Pitch must be [-8_192, 8_191]");
+		}
+
+		pitch += CENTER; // Now [0, 16_383]
+
+		msb = (byte)(pitch >> 7);
+		lsb = (byte)(pitch & 0x7F);
 	}
 
 	internal override byte GetCMDByte()
